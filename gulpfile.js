79d845db118d6e703b03adb59d97ef7ec07a640c
Silverfangs <silverfangs@users.noreply.github.com>
2017-06-25 11:05:53 +0800

    Releasing the first version

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    composer = require('gulp-uglify/composer'),
+    uglifyjsES = require('uglify-es');
 
+var minify = composer(uglifyjsES, console);
 
-gulp.task('default', function() {
-    gulp.src('./src/jquery.tapshow.js')
-        .pipe(uglify({
-            preserveComments: 'license'
+gulp.task('build', function() {
+    gulp.src('./src/*.js')
+        .pipe(minify({
+            warnings : true,
+            output: {
+                comments: '/^!/'
+            }
         }))
-        .pipe(rename({suffix: '.min'}))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist'));
+
+    // copy src script to dist
+    gulp.src('./src/*.js')
         .pipe(gulp.dest('./dist'));
 });
